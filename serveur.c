f26e69fd194561d4e09ed5fb1abaadf9ff18ec4b
Ray-ana <rayanazi305@gmail.com>
2025-12-29 16:55:03 +0100

    f

--- serveur.c
+++ serveur.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include <unistd.h>
 #include <signal.h>
 #include "ft_printf.h"
@@ -28,11 +29,13 @@ void	handle_signal(int sig, siginfo_t *info, void *context)
 	static int	count_byt;//se souvient du compte de bytes grace au static
 	pid_t	client;
 	
-	client = info->si_pid;
-	(void)context;
-	c <<= 1;
+	client = info->si_pid;//Grâce au flag SA_SIGINFO que tu mettras dans ton 
+	//sigaction, la structure info contient le PID du client. Cette ligne permet de savoir précisément qui t'envoie le signal.
+	(void)context;//(void) sert a utiliser le parametre context car on l'utilise nulle part dans le code
+	c <<= 1;//pour decaler les bytes a gauche laissant une case vide tout a droite car on recoit les bytes un par un et non en meme temps
+	//00000001| <<=1 0000001|0 <<=1 000001|00 <<=1 00001|000
 	if (sig == SIGUSR2)
-		c |= 1;
+		c |= 1;//rajoute le 1 tout a la fin
 	count_byt++;
 	if (count_byt == 8)
 	{
@@ -40,7 +43,7 @@ void	handle_signal(int sig, siginfo_t *info, void *context)
 			write(1, "\n", 1);
 		else
 			write(1, &c, 1);
-		c = 0;
+		c = 0;//Une fois la lettre affichée, on vide la boîte (c) et on remet le compteur à zéro pour pouvoir construire la lettre suivante.
 		count_byt = 0;
 	}
 	/*kill(info->si_pid, SIGUSR1); SI BONUS*/

f173eadcae95e1c7193b8fbbdd7d525213def4aa
Ray-ana <rayanazi305@gmail.com>
2026-01-07 21:36:42 +0100

    changer

--- serveur.c
+++ serveur.c
@@ -6,47 +6,40 @@
 /*   By: rzimaeva <rzimaeva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 12:25:26 by rzimaeva          #+#    #+#             */
-/*   Updated: 2025/12/29 16:37:59 by rzimaeva         ###   ########.fr       */
+/*   Updated: 2026/01/07 18:09:14 by rzimaeva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <unistd.h>
 #include <signal.h>
 #include "ft_printf.h"
 #include "minitalk.h"
 
-volatile sig_atomic_t	g_status; /*utiliser SIGINT pour le main*/
-
-void signal_handler(int status)
+//on utilise void handle(int sig, siginfo_t, void *) au lieu de void handle(int sig) car ca nous permet de recuperer le PID client
+//on laisse qund meme void *present pour que le compilateur fonctionne mais ca ne sert a rien dans minitalk
+void	sig_handler(int nbsig/*contient numero du signal 10 ou 12 voir explications.c*/, siginfo_t *info_pid, void *present)
 {
-    gSignalStatus = status;
-}
-
-void	handle_signal(int sig, siginfo_t *info, void *context)
-{
-	static char	c;//contient 8 bytes
-	static int	count_byt;//se souvient du compte de bytes grace au static
+	static char	c;//contient 8 BITS MAIS 1 seul BYTE
+	static int	count_bit;//se souvient du compte de bytes grace au static
 	pid_t	client;
 	
-	client = info->si_pid;//Grâce au flag SA_SIGINFO que tu mettras dans ton 
-	//sigaction, la structure info contient le PID du client. Cette ligne permet de savoir précisément qui t'envoie le signal.
-	(void)context;//(void) sert a utiliser le parametre context car on l'utilise nulle part dans le code
+	client = info_pid->si_pid;//si_pid fait partie de la structure siginfo_t
+	(void)present;//(void) sert a utiliser le parametre context car on l'utilise nulle part dans le code
 	c <<= 1;//pour decaler les bytes a gauche laissant une case vide tout a droite car on recoit les bytes un par un et non en meme temps
-	//00000001| <<=1 0000001|0 <<=1 000001|00 <<=1 00001|000
-	if (sig == SIGUSR2)
+	//1 = 00000001! (on decale a gauche) <<=1, 10 = 0000001!0 <<=1, 100 = 000001!00 <<=1, 1000 = 00001!000
+	if (nbsig == SIGUSR2)
 		c |= 1;//rajoute le 1 tout a la fin
-	count_byt++;
-	if (count_byt == 8)
+	count_bit++;
+	if (count_bit == 8)//total de bits voulu 
 	{
-		if (c == '\0')
+		if (c == '\0')//si on a atteint la fin des bits on va juste a la ligne
 			write(1, "\n", 1);
 		else
-			write(1, &c, 1);
+			write(1, &c, 1);//sinon on affiche le caractere voulu et on recommence avec un autre caractere
 		c = 0;//Une fois la lettre affichée, on vide la boîte (c) et on remet le compteur à zéro pour pouvoir construire la lettre suivante.
-		count_byt = 0;
+		count_bit = 0;
 	}
-	/*kill(info->si_pid, SIGUSR1); SI BONUS*/
+	/*kill(client, SIGUSR1); SI BONUS*/
 }
 
 int	main(void)
